aef2ad91666a2ba3cf013772a21c0650bb9e3641
hshimokawabe <h.shimokawabe@gmail.com>
2021-09-18 12:36:42 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
- get 'home' => 'homes#top'
- get 'books' => 'books#index'
- get "books/index"
-post 'books' => 'books#create'
- get 'books' => 'books#index'
- get "books/show"
- get "books/edit"
+get root to: "homes#top"
+resources :books
+ 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
